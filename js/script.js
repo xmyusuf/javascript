0a9875c3a605d0c610dd9ef6051783c456d467a2
xmyusuf <muhammadyusuf411042@gmail.com>
2022-04-09 12:35:58 +0500

    javascript

---
+++ js/script.js
@@ -0,0 +1,10 @@
+alert ("Xush kelibsiz!");
+
+var ism = prompt("Ismingiz nima?");
+var familiya = prompt("Familiyangiz nima?");
+var yosh = prompt("Yoshingiz nechida?");
+var ish = prompt("Ishlaysizmi yoki o'qiysizmi?");
+
+alert ("Raxmat😀");
+
+console.log(ism + "\n" + familiya + "\n" + yosh + "\n" + ish);
